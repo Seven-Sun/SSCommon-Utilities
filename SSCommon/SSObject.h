9269f1e446b303ebfe46ef70aebfa196a7f1bffd
SevenChan <qh_xiaochao@163.com>
2015-01-14 12:44:33 +0800

    重建工程

---
+++ SSCommon/SSObject.h
@@ -0,0 +1,13 @@
+//
+//  SSObject.h
+//  SSCommon
+//
+//  Created by Seven on 15-1-14.
+//  Copyright (c) 2015年 Allgateways. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSObject : NSObject
+
+@end
---
+++ SSCommon/SSObject.m
@@ -0,0 +1,13 @@
+//
+//  SSObject.m
+//  SSCommon
+//
+//  Created by Seven on 15-1-14.
+//  Copyright (c) 2015年 Allgateways. All rights reserved.
+//
+
+#import "SSObject.h"
+
+@implementation SSObject
+
+@end
