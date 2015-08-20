9b9c77ced5feef221fea54288efbf902422d5123
SevenChan <qh_xiaochao@163.com>
2015-08-20 10:03:10 +0800

    Merge categorys！

---
+++ SSCommon/Categorys/Categorys.h
@@ -0,0 +1,25 @@
+//
+//  Categorys.h
+//  MeChef
+//
+//  Created by Seven on 15/7/12.
+//  Copyright (c) 2015年 Fuego. All rights reserved.
+//
+
+#ifndef MeChef_Categorys_h
+#define MeChef_Categorys_h
+
+#import "UIColor+Theme.h"
+#import "NSString+UUID.h"
+#import "NSString+MD5.h"
+#import "UIFont+AppFont.h"
+#import "UIImage+LogN.h"
+#import "UIImage+Resize.h"
+#import "UIColor+Hex.h"
+#import "NSString+FirstLetter.h"
+#import "NSString+Verify.h"
+#import "NSDate+Formatter.h"
+#import "NSString+DateFromNow.h"
+
+
+#endif
---
+++ SSCommon/Categorys/NSString+Verify.h
@@ -0,0 +1,16 @@
+//
+//  NSString+Verify.h
+//  EShoping
+//
+//  Created by Seven on 14-11-29.
+//  Copyright (c) 2014年 FUEGO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Verify)
+
+-(BOOL)isEmailType;
+-(BOOL)isPhone;
+
+@end
